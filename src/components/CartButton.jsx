80e1fa5761326c2f010e06525f9620a80131c101
karim ali <karimalielsayed88@gmail.com>
2025-02-15 16:52:54 +0200

    cart component

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -4,7 +4,7 @@ import { FaCartShopping } from "react-icons/fa6";
 
 const CartButton = () => {
   return (
-    <div className="fixed right-9 my-5 bottom-10 p-4 rounded-full bg-mainColor shadow-2xl">
+    <div className="fixed right-9 my-1 bottom-10 p-4 rounded-full bg-mainColor shadow-2xl">
       <Link to={"/cart"}>
         <FaCartShopping size={30} color="white" />
       </Link>
