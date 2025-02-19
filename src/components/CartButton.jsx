426d2fc57f5e724c89be7f6f2c83bbb57d9d41f7
karim ali <karimalielsayed88@gmail.com>
2025-02-19 08:40:05 +0200

    add filteration on products

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -4,9 +4,10 @@ import { FaCartShopping } from "react-icons/fa6";
 
 const CartButton = () => {
   return (
-    <div className="fixed right-9 my-1 bottom-10 p-4 rounded-full bg-mainColor shadow-2xl">
-      <Link to={"/cart"}>
+    <div className="fixed right-9 my-1 bottom-10 p-4 rounded-full bg-mainColor shadow-2xl z-50">
+      <Link to={"/cart"} className="relative">
         <FaCartShopping size={30} color="white" />
+        <span className="absolute -top-3 -right-2 bg-black text-white text-md font-semibold block h-6 w-6 text-center  rounded-full">5</span>
       </Link>
     </div>
   );
--- src/components/ProductsComponent.jsx
+++ src/components/ProductsComponent.jsx
@@ -11,8 +11,10 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
 const ProductsComponent = () => {
+  const [selectedCategory, setSelectedCategory] = useState("")
   const { t } = useTranslation();
-  const products = useSelector((state) => state.products)
+  const products = useSelector((state) => state.products);
+  const categories = ["مشويات","صواني","ني","عزومات"]
   // const [products, setProducts] = useState([
   //   {
   //     image:
@@ -37,44 +39,53 @@ const ProductsComponent = () => {
   //   },
   // ]);
   return (
-    <div className="flex justify-center items-center gap-5 my-5 flex-wrap">
-      {products.map((product, index) => (
-        <Card className="w-80" key={index}>
-          <CardHeader shadow={false} floated={false} className="h-52">
-            <img
-              src={product.image}
-              alt="card-image"
-              className="h-52 w-full object-cover"
-            />
-          </CardHeader>
-          <CardBody>
-            <div className="mb-2 flex items-center justify-between">
-              <Typography color="blue-gray" className="font-medium text-lg ">
-                {product.name}
+    <div>
+      {/* filteration */}
+      <div className="flex justify-center items-center gap-5 my-5 flex-wrap">
+        {categories.map((cat,index) => (
+          <button key={index} onClick={() => setSelectedCategory(cat)} className={`${selectedCategory == cat ? "bg-secondColor" : "bg-gray-300"} font-medium text-lg py-2 px-8 text-black rounded-xl shadow-md`}>{cat}</button>
+        ))}
+      </div>
+      {/* products */}
+      <div className="flex justify-center items-center gap-5 my-5 flex-wrap">
+        {products.map((product, index) => (
+          <Card className="w-80" key={index}>
+            <CardHeader shadow={false} floated={false} className="h-52">
+              <img
+                src={product.image}
+                alt="card-image"
+                className="h-52 w-full object-cover"
+              />
+            </CardHeader>
+            <CardBody>
+              <div className="mb-2 flex items-center justify-between">
+                <Typography color="blue-gray" className="font-medium text-lg ">
+                  {product.name}
+                </Typography>
+                <Typography color="blue-gray" className="font-medium text-lg">
+                  {product.price}
+                </Typography>
+              </div>
+              <Typography
+                variant="small"
+                color="gray"
+                className="font-semibold opacity-75 text-lg text-start"
+              >
+                {product.description}
               </Typography>
-              <Typography color="blue-gray" className="font-medium text-lg">
-                {product.price}
-              </Typography>
-            </div>
-            <Typography
-              variant="small"
-              color="gray"
-              className="font-semibold opacity-75 text-lg text-start"
-            >
-              {product.description}
-            </Typography>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button
-              ripple={false}
-              fullWidth={true}
-              className="bg-mainColor text-white shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-            >
-              {t("addToCart")}
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button
+                ripple={false}
+                fullWidth={true}
+                className="bg-mainColor text-white shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+              >
+                {t("addToCart")}
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
