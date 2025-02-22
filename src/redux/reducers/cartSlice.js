3bbba10979b2590a366eeeb1ee72a9a70d4c09ef
karim ali <karimalielsayed88@gmail.com>
2025-02-22 21:56:56 +0200

    handle cart

--- src/redux/reducers/cartSlice.js
+++ src/redux/reducers/cartSlice.js
@@ -2,22 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [
-    {
-      image:
-        "https://img.youm7.com/ArticleImgs/2022/9/29/69151-%D9%83%D9%81%D8%AA%D8%A9-%D8%A7%D9%84%D8%AD%D8%A7%D8%AA%D9%8A.jpg",
-      name: "كفتة",
-      description: "4 صوابع وطبق رز ",
-      quantity: 5,
-      singlePrice: 100,
-    },
-    {
-      image:
-        "https://img.youm7.com/ArticleImgs/2022/9/29/69151-%D9%83%D9%81%D8%AA%D8%A9-%D8%A7%D9%84%D8%AD%D8%A7%D8%AA%D9%8A.jpg",
-      name: "كفتة",
-      description: "4 صوابع وطبق رز ",
-      quantity: 5,
-      singlePrice: 100,
-    },
   ],
 };
 
